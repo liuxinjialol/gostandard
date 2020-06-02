b8964c8c9f73f7a2d984c1c91a16616c12173314
liuxinjialol <liuxinjia@163.com>
2020-06-02 22:55:40 +0800

    1

--- day4/ptrgo.go
+++ day4/ptrgo.go
@@ -4,7 +4,7 @@ import (
     "fmt"
 )
 
-// this function changes reply:
+// Multiply this function changes reply:
 func Multiply(a, b int, reply *int) {
     *reply = a * b
 }
@@ -15,5 +15,12 @@ func main() {
     Multiply(10, 5, reply)
 	fmt.Println("Multiply:", *reply) // Multiply: 50
 	
-	fmt.Println("n=", n) // Multiply: 50
+    fmt.Println("n=", n) // Multiply: 50
+    
+
+    capitals := map[string] string {"France":"Paris", "Italy":"Rome", "Japan":"Tokyo" }
+    for key := range capitals {
+	    fmt.Println("Map item: Capital of", key, "is", capitals[key])
+    }
+
 }
